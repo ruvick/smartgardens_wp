1f50d424e68195b14631567fce86856470752738
Ruvick <rudikov.web@ya.ru>
2021-07-16 00:07:25 +0300

    Поправил поля в настройках темы

--- themes-fields.php
+++ themes-fields.php
@@ -12,28 +12,14 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field; 
 
 Container::make( 'theme_options', __( 'Настройки темы', 'crb' ) )
-    ->add_tab('Главная', array(
-      // Field::make( 'image', 'as_logo', 'Логотип в шапке')
-      //   ->set_width(30),
-      // Field::make( 'image', 'as_logo_white', 'Логотип в подвале')
-      //   ->set_width(30),
-      Field::make('text', 'about_home_title', 'Заголовок на главной'),
-      Field::make('rich_text', 'about_home', 'О нашей компании')
-    ))
-    ->add_tab('Акции', array(
-      Field::make('complex', 'complex_promo', 'Верхние блоки на главной')
-      // ->set_max(3) // Можно будет выбрать только 5 постов
-      ->add_fields(array(
-        Field::make('image', 'img_promo', 'Фото')
-        ->set_width(30),
-        Field::make('text', 'text_promo', 'Текст')   
-        ->set_width(30),
-        Field::make('text', 'sticker_promo', 'Стикер')   
-        ->set_width(30),
-        Field::make('text', 'link_promo', 'Ссылка')   
-        ->set_width(30),
-        ))
-    ))
+    // ->add_tab('Главная', array(
+    //   // Field::make( 'image', 'as_logo', 'Логотип в шапке')
+    //   //   ->set_width(30),
+    //   // Field::make( 'image', 'as_logo_white', 'Логотип в подвале')
+    //   //   ->set_width(30),
+    //   Field::make('text', 'about_home_title', 'Заголовок на главной'),
+    //   Field::make('rich_text', 'about_home', 'О нашей компании')
+    // ))
     ->add_tab('Контакты', array(
         Field::make( 'text', 'as_company', __( 'Название' ) )
           ->set_width(50),
