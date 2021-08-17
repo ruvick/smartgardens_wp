63c00acabe3d6541092225f152201e28b79a7974
Ruvick <rudikov.web@ya.ru>
2021-08-17 15:35:35 +0300

    Описание товар на главной из Наст темы

--- index.php
+++ index.php
@@ -1,209 +1,201 @@
 <?php get_header(); ?>
 
-<?php get_template_part('template-parts/header-section');?>
+<?php get_template_part('template-parts/header-section'); ?>
 
 <main class="page">
-			<section id="first" class="index-first first">
-				<div class="container">
+	<section id="first" class="index-first first">
+		<div class="container">
 
-					<div class="first__flex-block d-flex">
-						<div class="first__descrip">
-							<h1>
-								Гидропонная установка <br>  
-								у вас дома 
-							</h1>
-							<div class="first__price-block d-flex">
-								<div class="first__price-old">5500 <span>руб.</span></div>
-								<div class="first__price-new">2500 <span>руб.</span></div>
-							</div>
-							<p>
-								Выращивайте свежую зелень прямо у себя дома <br>
-								вне зависимости от времени года и погоды на улице
-							</p>
-							<a href="<?php echo get_permalink(45);?>" class="first__btn btn">ПОДРОБНЕЕ</a>
-						</div>
-						<div class="first__slider">
-							<div class="first__slider-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/slider-product/sl-prod-01.png" alt="">
-								<!-- <img src="<?php echo get_template_directory_uri();?>/img/shadow-sl.png" alt=""> -->
-							</div>
-							<div class="first__slider-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/slider-product/sl-prod-02.png" alt="">
-								<!-- <img src="<?php echo get_template_directory_uri();?>/img/shadow-sl.png" alt=""> -->
-							</div>
-							<div class="first__slider-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/slider-product/sl-prod-03.png" alt="">
-								<!-- <img src="<?php echo get_template_directory_uri();?>/img/shadow-sl.png" alt=""> -->
-							</div>
-						</div>
+			<div class="first__flex-block d-flex">
+				<div class="first__descrip">
+					<h1><?php echo carbon_get_theme_option('product_home_title'); ?></h1>
+					<div class="first__price-block d-flex">
+						<div class="first__price-old"><?php echo carbon_get_theme_option('product_old_price'); ?> <span>руб.</span></div>
+						<div class="first__price-new"><?php echo carbon_get_theme_option('product_new_price'); ?> <span>руб.</span></div>
+					</div>
+					<p><?php echo carbon_get_theme_option('product_short_descp'); ?></p>
+					<a href="<?php echo get_permalink(45); ?>" class="first__btn btn">ПОДРОБНЕЕ</a>
+				</div>
+				<div class="first__slider">
+					<div class="first__slider-img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/slider-product/sl-prod-01.png" alt="">
+						<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/shadow-sl.png" alt=""> -->
+					</div>
+					<div class="first__slider-img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/slider-product/sl-prod-02.png" alt="">
+						<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/shadow-sl.png" alt=""> -->
+					</div>
+					<div class="first__slider-img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/slider-product/sl-prod-03.png" alt="">
+						<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/shadow-sl.png" alt=""> -->
 					</div>
 				</div>
-			</section>
+			</div>
+		</div>
+	</section>
 
-			<section id="about" class="about">
-				<img src="<?php echo get_template_directory_uri();?>/img/sheet-01.png" class="about-bg-1" alt="">
-				<img src="<?php echo get_template_directory_uri();?>/img/sheet-02.png" class="about-bg-2" alt="">
-				<img src="<?php echo get_template_directory_uri();?>/img/sheet-03.png" class="about-bg-3" alt="">
-				<div class="container">
-					<h2>О нашем устройстве</h2>
-					<p>
-						Мы предлагаем Вашему вниманию стационарную гидропонную установку для выращивания зелени в домашних условиях.
-						Установка проста в эксплуатации и не требует специфических навыков при использовании. В установке
-						предусмотрено 12 ячеек для выращивания растений, имеются все необходимые режимы освещения с автоматической
-						регулировкой. Блок светодиодных ламп вынесены на штангу с регулировкой уровня 50 см. Установка снабжена
-						насосом для обеспечения циркуляции жидкости внутри бака, который в свою очередь снабжен буйком для индикации
-						уровня.
-					</p>
-					<h3>Технические характеристики</h3>
+	<section id="about" class="about">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/sheet-01.png" class="about-bg-1" alt="">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/sheet-02.png" class="about-bg-2" alt="">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/sheet-03.png" class="about-bg-3" alt="">
+		<div class="container">
 
-					<div class="about__charact charact__row">
+			<? $product_descp = carbon_get_theme_option("product_descp");
+			if (!empty($product_descp)) { ?>
+				<h2><?php echo carbon_get_theme_option('product_title'); ?></h2>
+				<p><? echo $product_descp; ?></p>
+			<? } ?>
 
-						<div class="charact__line d-flex">
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Мощность</div>
-								<div class="charact__property">60 Вт</div>
-							</div>
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Индекс цветопередачи</div>
-								<div class="charact__property">80 Ra</div>
-							</div>
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Поддержка димера</div>
-								<div class="charact__property">Да</div>
-							</div>
-						</div>
+			<h3>Технические характеристики</h3>
 
-						<div class="charact__line d-flex">
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Напряжение</div>
-								<div class="charact__property">110 В - 240 В</div>
-							</div>
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Световой поток</div>
-								<div class="charact__property">23029 Лм</div>
-							</div>
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Материал корпуса</div>
-								<div class="charact__property">Алюминий</div>
-							</div>
-						</div>
+			<div class="about__charact charact__row">
 
-						<div class="charact__line d-flex">
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Сертификация</div>
-								<div class="charact__property">Ce, ETL, FCC, RoHS</div>
-							</div>
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Количество светодиодов</div>
-								<div class="charact__property">120</div>
-							</div>
-							<div class="charact__tab d-flex">
-								<div class="charact__value">Цвета светодиодов</div>
-								<div class="charact__property">2835 белый + красный + синий</div>
-							</div>
-						</div>
+				<div class="charact__line d-flex">
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Мощность</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_home_power'); ?> Вт</div>
+					</div>
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Индекс цветопередачи</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_color_rendering_index'); ?> Ra</div>
+					</div>
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Поддержка димера</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_dimer_support'); ?></div>
+					</div>
+				</div>
 
+				<div class="charact__line d-flex">
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Напряжение</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_voltage'); ?></div>
+					</div>
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Световой поток</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_light_flow'); ?> Лм</div>
+					</div>
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Материал корпуса</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_body_material'); ?></div>
+					</div>
+				</div>
+
+				<div class="charact__line d-flex">
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Сертификация</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_certification'); ?></div>
+					</div>
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Количество светодиодов</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_number_leds'); ?></div>
+					</div>
+					<div class="charact__tab d-flex">
+						<div class="charact__value">Цвета светодиодов</div>
+						<div class="charact__property"><?php echo carbon_get_theme_option('product_led_colors'); ?></div>
+					</div>
+				</div>
+
+			</div>
+
+			<h3>Видеопрезентация</h3>
+			<a href="<?php echo get_template_directory_uri(); ?>/img/video/video.mp4" data-rel="media" class="about__link-video fancybox position">
+				<video class="about__video" controls="controls" loop autoplay muted poster="<?php echo get_template_directory_uri(); ?>/img/video/video.png">
+					<source src="<?php echo get_template_directory_uri(); ?>/img/video/video.mp4" type='video/ogg; codecs="theora, vorbis"'>
+				</video>
+			</a>
+
+			<h3>Галерея</h3>
+			<div class="about__gallery-row d-flex">
+
+				<a href="<?php echo get_template_directory_uri(); ?>/img/gallery/01.jpg" rel="lightbox" class="about__gallery-img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gallery/01.jpg" alt="">
+				</a>
+
+				<a href="<?php echo get_template_directory_uri(); ?>/img/gallery/02.jpg" rel="lightbox" class="about__gallery-img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gallery/02.jpg" alt="">
+				</a>
+
+				<a href="<?php echo get_template_directory_uri(); ?>/img/gallery/03.jpg" rel="lightbox" class="about__gallery-img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gallery/03.jpg" alt="">
+				</a>
+
+				<a href="<?php echo get_template_directory_uri(); ?>/img/gallery/04.jpg" rel="lightbox" class="about__gallery-img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gallery/04.jpg" alt="">
+				</a>
+
+				<a href="<?php echo get_template_directory_uri(); ?>/img/gallery/05.jpg" rel="lightbox" class="about__gallery-img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gallery/05.jpg" alt="">
+				</a>
+
+				<a href="<?php echo get_template_directory_uri(); ?>/img/gallery/06.jpg" rel="lightbox" class="about__gallery-img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gallery/06.jpg" alt="">
+				</a>
+
+			</div>
+
+			<div id="advant" class="advantages-block">
+				<h2>Преимущества</h2>
+
+				<div class="advantages-block__row d-flex">
+
+					<div class="advantages-block__item">
+						<div class="advantages-block__item-icon advantages-block__item-1"></div>
+						<p>Компактность</p>
 					</div>
 
-					<h3>Видеопрезентация</h3>
-					<a href="<?php echo get_template_directory_uri();?>/img/video/video.mp4" data-rel="media" class="about__link-video fancybox position">
-						<video class="about__video" controls="controls" loop autoplay muted poster="<?php echo get_template_directory_uri();?>/img/video/video.png">
-							<source src="<?php echo get_template_directory_uri();?>/img/video/video.mp4" type='video/ogg; codecs="theora, vorbis"'>
-						</video>
-					</a>
-
-					<h3>Галерея</h3>
-					<div class="about__gallery-row d-flex">
-
-						<a href="<?php echo get_template_directory_uri();?>/img/gallery/01.jpg" rel="lightbox" class="about__gallery-img"> 
-							<img src="<?php echo get_template_directory_uri();?>/img/gallery/01.jpg" alt=""> 
-						</a>
-
-						<a href="<?php echo get_template_directory_uri();?>/img/gallery/02.jpg" rel="lightbox" class="about__gallery-img"> 
-							<img src="<?php echo get_template_directory_uri();?>/img/gallery/02.jpg" alt=""> 
-						</a>
-
-						<a href="<?php echo get_template_directory_uri();?>/img/gallery/03.jpg" rel="lightbox" class="about__gallery-img"> 
-							<img src="<?php echo get_template_directory_uri();?>/img/gallery/03.jpg" alt="">
-						</a>
-
-						<a href="<?php echo get_template_directory_uri();?>/img/gallery/04.jpg" rel="lightbox" class="about__gallery-img"> 
-							<img src="<?php echo get_template_directory_uri();?>/img/gallery/04.jpg" alt="">
-						</a>
-
-						<a href="<?php echo get_template_directory_uri();?>/img/gallery/05.jpg" rel="lightbox" class="about__gallery-img"> 
-							<img src="<?php echo get_template_directory_uri();?>/img/gallery/05.jpg" alt="">
-						</a>
-
-						<a href="<?php echo get_template_directory_uri();?>/img/gallery/06.jpg" rel="lightbox" class="about__gallery-img"> 
-							<img src="<?php echo get_template_directory_uri();?>/img/gallery/06.jpg" alt="">
-						</a>
-
+					<div class="advantages-block__item">
+						<div class="advantages-block__item-icon advantages-block__item-2"></div>
+						<p>Вместительность</p>
 					</div>
 
-					<div id="advant" class="advantages-block">
-						<h2>Преимущества</h2>
+					<div class="advantages-block__item">
+						<div class="advantages-block__item-icon advantages-block__item-3"></div>
+						<p>Малая мощность</p>
+					</div>
 
-						<div class="advantages-block__row d-flex">
-
-							<div class="advantages-block__item">
-								<div class="advantages-block__item-icon advantages-block__item-1"></div>
-								<p>Компактность</p>
-							</div>
-
-							<div class="advantages-block__item">
-								<div class="advantages-block__item-icon advantages-block__item-2"></div>
-								<p>Вместительность</p>
-							</div>
-
-							<div class="advantages-block__item">
-								<div class="advantages-block__item-icon advantages-block__item-3"></div>
-								<p>Малая мощность</p>
-							</div>
-
-							<div class="advantages-block__item">
-								<div class="advantages-block__item-icon advantages-block__item-4"></div>
-								<p>Безопасность</p>
-							</div>
-
-						</div>
+					<div class="advantages-block__item">
+						<div class="advantages-block__item-icon advantages-block__item-4"></div>
+						<p>Безопасность</p>
 					</div>
 
 				</div>
-			</section>
+			</div>
 
-			<section id="products" class="products">
-				<div class="container">
-					<h2>Аксессуары</h2>
+		</div>
+	</section>
 
-					<div class="prod-card d-flex">
+	<section id="products" class="products">
+		<div class="container">
+			<h2>Аксессуары</h2>
 
-						<?
-							$args = array(
-							'posts_per_page' => 6,
-							'post_type' => 'ultra',
-							'tax_query' => array(
-								array(
-									'taxonomy' => 'ultracat',
-									'field' => 'id',
-									'terms' => array(3)
-									)
-								)
-							);
-							$query = new WP_Query($args);
+			<div class="prod-card d-flex">
 
-								foreach( $query->posts as $post ){
-								$query->the_post();
-								get_template_part('template-parts/product-elem');
-							}  
-							wp_reset_postdata(); 
-						?>
+				<?
+				$args = array(
+					'posts_per_page' => 6,
+					'post_type' => 'ultra',
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'ultracat',
+							'field' => 'id',
+							'terms' => array(3)
+						)
+					)
+				);
+				$query = new WP_Query($args);
 
-					</div>
+				foreach ($query->posts as $post) {
+					$query->the_post();
+					get_template_part('template-parts/product-elem');
+				}
+				wp_reset_postdata();
+				?>
 
-				</div>
-			</section>
+			</div>
 
-		</main>
+		</div>
+	</section>
 
-<?php get_footer(); ?>  
+</main>
+
+<?php get_footer(); ?>
